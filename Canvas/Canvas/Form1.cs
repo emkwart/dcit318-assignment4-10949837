1b2bd8ffd005a6461a2bf00854446f15752f0ed8
emkwart <102715288+emkwart@users.noreply.github.com>
2024-07-09 16:34:41 +0000

    Add files via upload
    A simple Canvas C# application

---
+++ Canvas/Canvas/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Canvas
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point previousPoint;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                previousPoint = e.Location;
+            }
+        }
+
+        private void drawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = drawingPanel.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, previousPoint, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+
+        private void drawingPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+    }
+}
