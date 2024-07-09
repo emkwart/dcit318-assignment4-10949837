aacb6577d18c705c272134be590f19c5ed6aebea
emkwart <102715288+emkwart@users.noreply.github.com>
2024-07-09 16:32:45 +0000

    Add files via upload
    A simple LogIn form application using C#

---
+++ LogIn Form/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Button logInBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUserName = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.logInBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textUserName.Location = new System.Drawing.Point(213, 83);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(174, 20);
+            this.textUserName.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textPassword.Location = new System.Drawing.Point(213, 135);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(174, 20);
+            this.textPassword.TabIndex = 1;
+            this.textPassword.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.logInBtn.Location = new System.Drawing.Point(261, 197);
+            this.logInBtn.Name = "button1";
+            this.logInBtn.Size = new System.Drawing.Size(75, 23);
+            this.logInBtn.TabIndex = 2;
+            this.logInBtn.Text = "Login";
+            this.logInBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter UserName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logInBtn);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textUserName);
+            this.Name = "Form1";
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ LogIn Form/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textUserName(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textPassword(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logInBtn(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Information");
+
+        }
+
+        private void label1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
